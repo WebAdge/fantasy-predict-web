03c45b77e82a92fcd0fb27901c4edec6d3c3e16b
Aderemi Dare <richardjohn740@gmail.com>
2025-11-13 16:04:13 +0100

    updated sp

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -4,7 +4,6 @@ import { appLogout } from "../utils/shared";
 import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { LogOut, User, Home, UserCheck } from "react-feather";
 
-
 const ChatHeader = () => {
   const navigate = useNavigate();
   const popUpRef = useRef<HTMLDivElement>(null);
@@ -19,7 +18,6 @@ const ChatHeader = () => {
       setOpen(false);
     }
   };
-  
 
   useEffect(() => {
     // @ts-ignore
@@ -45,7 +43,7 @@ const ChatHeader = () => {
             {open ? (
               <div className="flyover-menu right-auto top-[50px] rounded-md w-[250px]">
                 <ul>
-                <li
+                  <li
                     className="flex gap-2 cursor-pointer hover:opacity-70 py-1"
                     onClick={() => navigate("/dashboard")}
                   >
